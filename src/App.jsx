7a3a498bc231bfd1c5ae298c760590ece3efe916
EsinEraslann <esineraslanx@gmail.com>
2024-05-11 23:56:00 +0300

    homework fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './style.scss'
+/* import './style.scss' */
+import "./App.css";
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+display: flex;
+align-items: end;
+`;
+
+const ListItem = styled.li`
+cursor: pointer;
+`;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoInput, setTodoInput] = useState("");
+
+  const [todos, setTodos] = useState([]);
+
+ // console.log(todos);
+
+  const addTodo = () => {
+    setTodos([...todos, todoInput]); // arraye eleman ekleyip rest etme
+    setTodoInput("");
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Wrapper>
+    <Form>
+      <Form.Group className="" controlId="exampleForm.ControlInput1">
+        <Form.Label>Hedef</Form.Label>
+        <Form.Control 
+        value={todoInput}
+        onChange={(e) => {
+          setTodoInput(e.target.value);
+        }}
+        type="text" />
+      </Form.Group>
+    </Form>
+    <Button className="addButton" onClick={addTodo}>Ekle</Button>
+    </Wrapper>
+    <ul>
+      {/* <li>Test</li>
+      <li>Test</li> */}
+      {todos.map((todo) => (
+        <ListItem 
+        onClick={() => {
+        setTodos(todos.filter((filterTodo) => filterTodo !== todo));
+      }}
+        key={todo}>{todo}</ListItem>
+      ))}
+    </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
